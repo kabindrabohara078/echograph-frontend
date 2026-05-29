418bdcd19eda8560419b24e7bfc5c324ad6452b0
Kabindra Bohara <kabindrabohara1@gmail.com>
2026-05-29 18:47:08 +0530

    header positioned

--- src/assets/elements/MainMenu.jsx
+++ src/assets/elements/MainMenu.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./MainMenu.css";
-import GetStarted from '../../pages/GetStarted'
 
 export default function MainMenu(props) {
 
