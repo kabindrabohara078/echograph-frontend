d23532150c0ba75adc951aee270752239ba65421
Kabindra Bohara <kabindrabohara1@gmail.com>
2026-06-07 15:25:40 +0530

    add dark mode in menu mobile

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -15,31 +15,105 @@ export default function MainMenu(props) {
   return (
     <>
 
-     <div className={`mobile-menu ${props.open ? "show" : ""} top-20`}>
+     <div className={`mobile-menu ${props.open ? "show" : ""} top-20`}
+     style={{
+                    backgroundColor: props.dark? '#222':'white',
+                    color:props.dark? 'white':'black'
+
+     }}
+     >
         
-        <Link className="cursor-pointer" to='/products' onClick={() => props.setOpen(!props.open)}>Product</Link>
-          <Link to='/start' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Get Started</Link>
-          <Link to='/features' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Features</Link>
-          <Link to='/demo' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Demo Proview</Link>
+        <Link
+        style={{
+          color:props.dark? 'white':'black'
+        }}
+        className="cursor-pointer" to='/products' onClick={() => props.setOpen(!props.open)}>Product</Link>
+          <Link
+          style={{
+          color:props.dark? 'white':'black'
+        }}
+          to='/start' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Get Started</Link>
+          <Link
+          style={{
+          color:props.dark? 'white':'black'
+        }}
+          to='/features' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Features</Link>
+          <Link
+          style={{
+          color:props.dark? 'white':'black'
+        }}
+          to='/demo' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Demo Proview</Link>
 
-        <Link className="cursor-pointer">Memory Engine</Link>
-          <Link to='/memory/#search' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Semantic Search</Link>
-          <Link to='/memory/#vector' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Vector Retrieval</Link>
-          <Link to='/memory/#ranking' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Adaptive Ranking</Link>
-          <Link to='/memory/#lifecycle' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Memory Lifecycle</Link>
-          <Link to='/memory/#context' onClick={() => props.setOpen(!props.open)} className="inlink-padding">User Context</Link>
+        <Link
+        style={{
+          color:props.dark? 'white':'black'
+        }}
+        className="cursor-pointer">Memory Engine</Link>
+          <Link
+          style={{
+          color:props.dark? 'white':'black'
+        }}
+          to='/memory/#search' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Semantic Search</Link>
+          <Link
+          style={{
+          color:props.dark? 'white':'black'
+        }}
+          to='/memory/#vector' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Vector Retrieval</Link>
+          <Link 
+          style={{
+          color:props.dark? 'white':'black'
+        }}
+          to='/memory/#ranking' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Adaptive Ranking</Link>
+          <Link 
+          style={{
+          color:props.dark? 'white':'black'
+        }}
+          to='/memory/#lifecycle' onClick={() => props.setOpen(!props.open)} className="inlink-padding">Memory Lifecycle</Link>
+          <Link 
+          style={{
+          color:props.dark? 'white':'black'
+        }}
+          to='/memory/#context' onClick={() => props.setOpen(!props.open)} className="inlink-padding">User Context</Link>
         
 
-        <Link to="/docs" onClick={() => props.setOpen(!props.open)} className="cursor-pointer">Docs</Link>
-        <Link to="https://github.com/kabindrabohara078/echograph-frontend" target="_blank">Github</Link>
+        <Link 
+        style={{
+          color:props.dark? 'white':'black'
+        }}
+        to="/docs" onClick={() => props.setOpen(!props.open)} className="cursor-pointer">Docs</Link>
+        <Link 
+        style={{
+          color:props.dark? 'white':'black'
+        }}
+        to="https://github.com/kabindrabohara078/echograph-frontend" target="_blank">Github</Link>
         
 
-        <Link className="cursor-pointer">Company</Link>
-          <Link onClick={() => props.setOpen(!props.open)} to="/about" className="inlink-padding">About</Link>
-          <Link onClick={() => props.setOpen(!props.open)} to="/careers" className="inlink-padding">Careers</Link>
+        <Link 
+        style={{
+          color:props.dark? 'white':'black'
+        }}
+        className="cursor-pointer">Company</Link>
+          <Link 
+          style={{
+          color:props.dark? 'white':'black'
+        }}
+          onClick={() => props.setOpen(!props.open)} to="/about" className="inlink-padding">About</Link>
+          <Link 
+          style={{
+          color:props.dark? 'white':'black'
+        }}
+          onClick={() => props.setOpen(!props.open)} to="/careers" className="inlink-padding">Careers</Link>
 
-        <Link className="cursor-pointer" onClick={() => props.setOpen(!props.open)} to="/test" >Test</Link>
-        <Link className="cursor-pointer" onClick={() => {
+        <Link 
+        style={{
+          color:props.dark? 'white':'black'
+        }}
+        className="cursor-pointer" onClick={() => props.setOpen(!props.open)} to="/test" >Test</Link>
+        <Link 
+        style={{
+          color:props.dark? 'white':'black'
+        }}
+        className="cursor-pointer" onClick={() => {
           props.setDark(!props.dark)
           props.setOpen(false)
         }} > {props.dark? "Light mode":"Dark mode"} </Link>
